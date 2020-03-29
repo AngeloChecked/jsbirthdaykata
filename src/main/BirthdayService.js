315a92318e703395613869a25e331de934b398de
Angelo Ceccato <angelo.ceccato@it.clara.net>
2020-03-29 20:38:23 +0200

    use RxJs to handle streams in a more expressive way

--- src/main/BirthdayService.js
+++ src/main/BirthdayService.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const readline = require('readline');
+const { fromEvent } = require('rxjs');
+const { scan, takeUntil, takeLast} = require('rxjs/operators');
 
 module.exports = {
 
@@ -9,6 +12,34 @@ module.exports = {
             dateOfBirth: "1982/10/08", 
             email: "john.doe@foobar.com"
         };
+    },
+    readFileLine: (fileNamePath) =>{
+        const readInterface = readline.createInterface(fs.createReadStream(fileNamePath));
+        const source = fromEvent(readInterface, 'line');
+        const end$ = fromEvent(readInterface, 'close');
+
+        return new Promise((resolve,rejected) => 
+            source.pipe(
+                scan((acc, curr) => { acc.push(curr); return acc; }, []),
+                takeUntil(end$),
+                takeLast(1)
+            ).subscribe(lastAccumulateValue => resolve(lastAccumulateValue))
+        );
+    },
+    serializeLineToEmployee: (line, valueNames) =>{
+        let employeeValues = line.split(',')
+                .map(value => value.trim());
+
+
+                
+
+
+        let map = valueNames.reduce((acc, value, index) => {
+            acc[value] = employeeValues[index];
+            return acc;
+        },{});
+
+        return map;
     }
 
 };
--- src/test/findEmployeeByDate_test.js
+++ src/test/findEmployeeByDate_test.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 const {findEmployeeByDate} = require('../main/BirthdayService');
+const {readFileLine} = require('../main/BirthdayService');
+const {serializeLineToEmployee} = require('../main/BirthdayService');
 
     describe('find employee by date',  () => {
 
@@ -7,15 +9,45 @@ const {findEmployeeByDate} = require('../main/BirthdayService');
 
         });
 
-        it('should return employee', () => {
-            
-            let employee = findEmployeeByDate("1982/10/08");
+        it('should return file lines in array', async () => {
+            let fileLines = await readFileLine('src/resources/flat.txt');
+
+            assert.deepEqual(fileLines, [
+                'last_name, first_name, date_of_birth, email',
+                'Doe, John, 1982/10/08, john.doe@foobar.com',
+                'Ann, Mary, 1975/09/11, mary.ann@foobar.com'
+            ]);
+        });
+
+        it('should return employee form linestring', async () => {
+            let employee = serializeLineToEmployee('Doe, John, 1982/10/08, john.doe@foobar.com', ['lastName','firstName','dateOfBirth','email']);
+                            
 
             assert.deepEqual(employee, {
                 lastName: "Doe",
                 firstName: "John", 
                 dateOfBirth: "1982/10/08", 
                 email: "john.doe@foobar.com"
+            });
+        });
+
+        xit('should return employee', () => {
+            
+            let employee1 = findEmployeeByDate("1982/10/08");
+            let employee2 = findEmployeeByDate("1975/09/11");
+
+            assert.deepEqual(employee1, {
+                lastName: "Doe",
+                firstName: "John", 
+                dateOfBirth: "1982/10/08", 
+                email: "john.doe@foobar.com"
+            });
+
+            assert.deepEqual(employee2, {
+                lastName: "Ann",
+                firstName: "Mary", 
+                dateOfBirth: "1975/09/11", 
+                email: "mary.ann@foobar.com"
             });
         });
 
